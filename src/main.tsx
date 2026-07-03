ff9182f80634da8d7baade59d042b285a6c7f7b3
myselfwebdemo <maxim.pinewood@gmail.com>
2026-07-03 23:33:04 +0300

    Urgent fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Game from './game/game'
+import Game from './game/Game'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
